0fd2fde1b75607910b322281e8f446eb03742aa5
rakeshrewale <rakeshrewale993@gmail.com>
2022-07-04 14:19:20 +0530

    Added: generic class to take 3 variable of generic type

--- src/Com/Generics/Generics.java
+++ src/Com/Generics/Generics.java
@@ -1,24 +1,38 @@
 package Com.Generics;
 
-public class Generics {
-
+public class Generics<T extends Comparable<T>> {
 	
-public static void main(String[] args) {
-		
-		System.out.println("Maximum number out of three integers is: " + maximum(11, 18, 200));
-		System.out.println("Maximum number out of three float is: " + maximum(45f, 75f, 88f));
-		System.out.println("Maximum among three string is: " + maximum("Apple", "Peach", "Banana"));
+	T num1;
+	T num2;
+	T num3;
+
+	Generics(T num1, T num2, T num3) {
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
 	}
 
-	public static <T extends Comparable<T>> T maximum(T num1, T num2, T num3) {
+	public T maximum() {
 
-		if (num2.compareTo(num1) > 0) {
-			return num2;
-		} else if (num3.compareTo(num2) > 0) {
-			return num3;
-		} else {
+		if (num1.compareTo(num2) > 0) {
 			return num1;
+		} else if (num2.compareTo(num3) > 0) {
+			return num2;
+		} else {
+			return num3;
 		}
-
 	}
+
+	public static void main(String[] args) {
+		
+		Generics compareInteger = new Generics(2345, 12, 7);
+		System.out.println("Maximum number out of three integers is: " + compareInteger.maximum());
+		
+		Generics comapreFloat = new Generics(123.f, 345f, 678f);
+		System.out.println("Maximum number out of three float is: " + comapreFloat.maximum());
+		
+		Generics compareString = new Generics("Apple", "Orange", "potato");
+		System.out.println("Maximum among three string is: " + compareString.maximum());
+	}
+
 }
