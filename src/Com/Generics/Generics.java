8601478129d402db19b524d9ad157c6f5058f7be
rakeshrewale <rakeshrewale993@gmail.com>
2022-07-04 13:59:30 +0530

    Added: Pls find max string names

--- src/Com/Generics/Generics.java
+++ src/Com/Generics/Generics.java
@@ -2,23 +2,27 @@ package Com.Generics;
 
 public class Generics {
 
-	public static float compareTo(float num1, float num2, float num3) {
+	
+		public static String compareTO(String name1, String name2, String name3) {
 
-		if (num1 > num2 && num1 > num3) {
-			return num1;
-		} else if (num2 > num3) {
-			return num2;
-		} else {
-			return num3;
+			int num1 = name1.compareTo(name2);
+			int num2 = name1.compareTo(name3);
+			int num3 = name2.compareTo(name3);
+
+			if (num1 > num2 && num1 > num2) {
+				return name1;
+			} else if (num2 > num3) {
+				return name2;
+			} else {
+				return name3;
+			}
 		}
-	}
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
+			Generics value = new Generics();
+			System.out.println("max Number for 1st test case=" + value.compareTO("Apple", "banana", "peach"));
+			System.out.println("max Number for 2nd test case=" + value.compareTO("peach", "banana", "peach"));
+			System.out.println("max Number for 3rd test case=" + value.compareTO("banana", "apple", "orange"));																								// position.
+		}
 
-		Generics number = new Generics();
-		System.out.println("max Number for 1st test case=" + number.compareTo(4.1f, 2.6f, 1.7f));
-		
-		System.out.println("max Number for 2nd test case=" + number.compareTo(2.7f, 6.0f, 3.70f));
-		System.out.println("max Number for 3rd test case=" + number.compareTo(1.1f, 2.1f, 2.2f));
 	}
-}
