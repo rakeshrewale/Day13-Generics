31f952241745838426001e3afb09c422683640a6
rakeshrewale <rakeshrewale993@gmail.com>
2022-07-04 14:08:32 +0530

    Added: interger,float,string to one generic method and find the max

--- src/Com/Generics/Generics.java
+++ src/Com/Generics/Generics.java
@@ -3,26 +3,22 @@ package Com.Generics;
 public class Generics {
 
 	
-		public static String compareTO(String name1, String name2, String name3) {
+public static void main(String[] args) {
+		
+		System.out.println("Maximum number out of three integers is: " + maximum(11, 18, 200));
+		System.out.println("Maximum number out of three float is: " + maximum(45f, 75f, 88f));
+		System.out.println("Maximum among three string is: " + maximum("Apple", "Peach", "Banana"));
+	}
 
-			int num1 = name1.compareTo(name2);
-			int num2 = name1.compareTo(name3);
-			int num3 = name2.compareTo(name3);
+	public static <T extends Comparable<T>> T maximum(T num1, T num2, T num3) {
 
-			if (num1 > num2 && num1 > num2) {
-				return name1;
-			} else if (num2 > num3) {
-				return name2;
-			} else {
-				return name3;
-			}
-		}
-
-		public static void main(String[] args) {
-			Generics value = new Generics();
-			System.out.println("max Number for 1st test case=" + value.compareTO("Apple", "banana", "peach"));
-			System.out.println("max Number for 2nd test case=" + value.compareTO("peach", "banana", "peach"));
-			System.out.println("max Number for 3rd test case=" + value.compareTO("banana", "apple", "orange"));																								// position.
+		if (num2.compareTo(num1) > 0) {
+			return num2;
+		} else if (num3.compareTo(num2) > 0) {
+			return num3;
+		} else {
+			return num1;
 		}
 
 	}
+}
