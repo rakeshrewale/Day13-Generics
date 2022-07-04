577d535432dd0f3cb82a1cd0af82e97d97805c7f
rakeshrewale <rakeshrewale993@gmail.com>
2022-07-04 13:37:08 +0530

    Added: Please check max interger number

--- src/Com/Generics/Generics.java
+++ src/Com/Generics/Generics.java
@@ -2,8 +2,27 @@ package Com.Generics;
 
 public class Generics {
 
+public static Integer compareTo(Integer num1, Integer num2, Integer num3) {
+		
+		if (num1 > num2 && num1 > num3) {
+			return num1;
+		} else if (num2 > num3) {
+			return num2;
+		} else {
+			return num3;
+		}
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Generics integer = new Generics();
+		
+		System.out.println("max Number for 1st test case=" + integer.compareTo(5, 3, 2));// Test case 1 :given max number for
+																					// 1st position.
+		System.out.println("max Number for 2nd test case=" + integer.compareTo(2, 6, 3));// Test case 2:given max number for
+																					// 2nd position.
+		System.out.println("max Number for 3rd test case=" + integer.compareTo(1, 2, 3));// Test case 3:given max number for
+																					// the 3rd position.
 
 	}
 
